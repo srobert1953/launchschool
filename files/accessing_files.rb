5738f43d44eaa98e8ada81941de94652021ba73f
srobert1953 <srobert1953@gmail.com>
2016-10-31 15:01:43 +0100

    File finished

--- files/accessing_files.rb
+++ files/accessing_files.rb
@@ -3,8 +3,19 @@ path = File.join(File.dirname(__FILE__), '..', 'files txt')
 
 file = File.new(File.join(path, 'irb_test.txt'), 'w')
 
+file.puts "abcd"
+file.print "efgh"
+file.write "ijkl"
+file << "mnop"
+
 file.close
 
-File.open(File.join(path, 'irb_test.txt'), 'r' ) do
-  # Some code
+File.open(File.join(path, 'irb_test.txt'), 'r' ) do |file|
+  while line = file.gets
+    puts "** " + line.chomp.reverse + " **"
+  end
+end
+
+File.open(File.join(path, 'irb_test.txt'), 'r') do |file|
+  file.each { |line| puts line.upcase }
 end
--- files/files.rb
+++ files/files.rb
@@ -1,6 +1,39 @@
 # files.rb
 
-puts __FILE__
-puts File.expand_path(__FILE__)
-puts File.dirname(__FILE__)
-puts File.join(File.dirname(__FILE__), '..', 'files\ txt')
+# puts __FILE__
+# puts File.expand_path(__FILE__)
+# puts File.dirname(__FILE__)
+# puts File.join(File.dirname(__FILE__), '..', 'files\ txt')
+
+File.open('simple_file.txt', 'w') { |file| file.write("Adding first line.")}
+
+File.open('simple_file.txt', 'a+') do |file|
+  file << "On new line?"        # Puts the text on the same line
+end
+
+File.open('simple_file.txt', 'a+') do |file|
+  file.puts "New line please!"   # Puts the text on the same line
+end
+
+File.open('simple_file.txt', "a+") do |file|
+  file.write("Please write a new line") # Puts the text on a new line
+end
+
+File.readlines('simple_file.txt').each do |line|
+  puts line
+end
+
+File.open('some_tests.txt', 'w') do |file|
+  x = 1
+  loop do
+    file.puts "#{x}. Line number #{x}"
+    x += 1
+    if x > 10
+      break
+    end
+  end
+end
+
+File.readlines('some_tests.txt').each do |line|
+  puts line
+end
